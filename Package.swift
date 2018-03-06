f80db02cd7a67a3dbe428256e40d6f537f53051d
Daniel Thorpe <danthorpe@me.com>
2018-03-06 23:14:24 +0000

    Updates Package.swift

--- Package.swift
+++ Package.swift
@@ -1,4 +1,57 @@
+// swift-tools-version:4.0
+
 import PackageDescription
+
+let pkg = Package(name: "ProcedureKit")
+
+pkg.products = [
+    .library(name: "ProcedureKit", targets: ["ProcedureKit"]),
+    .library(name: "ProcedureKitCloud", targets: ["ProcedureKitCloud"]),
+    .library(name: "ProcedureKitLocation", targets: ["ProcedureKitLocation"]),
+    .library(name: "ProcedureKitMac", targets: ["ProcedureKitMac"]),
+    .library(name: "ProcedureKitNetwork", targets: ["ProcedureKitNetwork"]),
+    .library(name: "TestingProcedureKit", targets: ["TestingProcedureKit"])
+]
+
+pkg.targets = [
+    .target(name: "ProcedureKit",
+            path: "Sources/ProcedureKit"),
+    .target(name: "ProcedureKitCloud",
+            dependencies: ["ProcedureKit"],
+            path: "Sources/ProcedureKitCloud"),
+    .target(name: "ProcedureKitLocation",
+            dependencies: ["ProcedureKit"],
+            path: "Sources/ProcedureKitLocation"),
+    .target(name: "ProcedureKitMac",
+            dependencies: ["ProcedureKit"],
+            path: "Sources/ProcedureKitMac"),
+    .target(name: "ProcedureKitNetwork",
+            dependencies: ["ProcedureKit"],
+            path: "Sources/ProcedureKitNetwork"),
+    .target(name: "TestingProcedureKit",
+            dependencies: ["ProcedureKit"],
+            path: "Sources/TestingProcedureKit"),
+    .testTarget(name: "ProcedureKitTests",
+                dependencies: ["ProcedureKit", "TestingProcedureKit"],
+                path: "Tests/ProcedureKitTests"),
+    .testTarget(name: "ProcedureKitStressTests",
+                dependencies: ["ProcedureKit", "TestingProcedureKit"],
+                path: "Tests/ProcedureKitStressTests"),
+    .testTarget(name: "ProcedureKitCloudTests",
+                dependencies: ["ProcedureKitCloud", "TestingProcedureKit"],
+                path: "Tests/ProcedureKitCloudTests"),
+    .testTarget(name: "ProcedureKitLocationTests",
+                dependencies: ["ProcedureKitLocation", "TestingProcedureKit"],
+                path: "Tests/ProcedureKitLocationTests"),
+    .testTarget(name: "ProcedureKitMacTests",
+                dependencies: ["ProcedureKitMac", "TestingProcedureKit"],
+                path: "Tests/ProcedureKitMacTests"),
+    .testTarget(name: "ProcedureKitNetworkTests",
+                dependencies: ["ProcedureKitNetwork", "TestingProcedureKit"],
+                path: "Tests/ProcedureKitNetworkTests"),
+]
+
+/*
 
 let package = Package(
     name: "ProcedureKit",
@@ -63,3 +116,5 @@ let package = Package(
         "Tests/ProcedureKitTVTests",
     ]
 )
+
+ */
